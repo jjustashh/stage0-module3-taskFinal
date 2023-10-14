6d229c04f2579d6cf7bb553a54bc6fa12f61925e
Donat <donatborkovskij@gmail.com>
2023-10-14 16:29:49 +0200

    commit

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int digit1 = number % 10;
+        int digit2 = (number / 10) % 10;
+        int digit3 = (number / 100) % 10;
+        int digit4 = number / 1000;
+
+        System.out.println(digit1 + digit2 + digit3 + digit4);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -2,6 +2,6 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumbersPrinter {
     public void printIsPositive(int number) {
-
+        System.out.println((number + Math.abs(number)) / 2 == number && number != 0);
     }
 }
